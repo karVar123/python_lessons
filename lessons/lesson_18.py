a8ee505263682f5c76ea908243dfd6e19c1f66c6
KarenVardanyan03 <vardanyankaren924@gmail.com>
2022-04-21 17:55:26 +0400

    adding files

---
+++ lessons/lesson_18.py
@@ -0,0 +1,23 @@
+class Home:
+    def __init__(self):
+        pass
+
+    def fun1(self):
+        return "fun 1 is working "
+
+    def fun2(self):
+        return "fun 2 is working "
+
+    def fun3(self):
+        return "fun 4 is working "
+
+    def fun4(self):
+        return "fun 4 is working "
+
+
+home = Home()
+
+print(home.fun1())
+print(home.fun2())
+print(home.fun3())
+print(home.fun4())
